2929edb5447240fcece89486ac03898f5ca40915
Nat-Reid <nathanielreid@gmail.com>
2019-01-04 16:12:18 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  if (num.abs <= 1)
+    return false
+  end
+
+  a = 2
+
+  while (a <= num.abs**(0.5)) do
+    if ((num%a) == 0)
+      return false
+    end
+    a += 1
+  end
+  
+  true
+end
